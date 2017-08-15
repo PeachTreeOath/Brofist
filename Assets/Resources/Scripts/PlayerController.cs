07a0d6fa6b3bf088a3c919371d0e92df6b6de205
PeachTreeOath <peachtreeoath@gmail.com>
2017-08-15 00:47:30 -0600

    Player movement

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -1,26 +1,95 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    private bool isFacingRight;
+    public float speed;
+    public bool isFacingRight;
+
     private Rigidbody2D body;
+    private PlayerState state;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         body = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetAxisRaw("Horizontal") > 0)
-        {
+        state = PlayerState.STANDING;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateState();
+        UpdatePosition();
+    }
+
+    private void UpdateState()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            if (isFacingRight)
+                state = PlayerState.WALKING_FORWARD;
+            else
+                state = PlayerState.WALKING_BACKWARD;
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
+            if (isFacingRight)
+                state = PlayerState.WALKING_BACKWARD;
+            else
+                state = PlayerState.WALKING_FORWARD;
+        }
+        else
+        {
+            state = PlayerState.STANDING;
+        }
+    }
 
+    private void UpdatePosition()
+    {
+        switch (state)
+        {
+            case PlayerState.STANDING:
+                break;
+            case PlayerState.BLOCKING:
+                break;
+            case PlayerState.WALKING_BACKWARD:
+                {
+                    float delta = (isFacingRight ? -1 : 1) * speed * Time.deltaTime;
+                    transform.position += new Vector3(delta, 0);
+                }
+                break;
+            case PlayerState.WALKING_FORWARD:
+                {
+                    float delta = (isFacingRight ? 1 : -1) * speed * Time.deltaTime;
+                    transform.position += new Vector3(delta, 0);
+                }
+                break;
+            case PlayerState.DASHING_BACK:
+                break;
+            case PlayerState.DASHING_FORWARD:
+                break;
+            case PlayerState.JUMPING_UP:
+                break;
+            case PlayerState.JUMPING_BACK:
+                break;
+            case PlayerState.JUMPING_FORWARD:
+                break;
+            case PlayerState.DASH_JUMPING_BACK:
+                break;
+            case PlayerState.DASH_JUMPING_FORWARD:
+                break;
+            case PlayerState.AIRDASHING_BACK:
+                break;
+            case PlayerState.AIRDASHING_FORWARD:
+                break;
+            case PlayerState.ATTACKING:
+                break;
+            default:
+                break;
         }
     }
 }
---
+++ Assets/Resources/Scripts/PlayerState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    STANDING,
+    BLOCKING,
+    WALKING_BACKWARD,
+    WALKING_FORWARD,
+    DASHING_BACK,
+    DASHING_FORWARD,
+    JUMPING_UP,
+    JUMPING_BACK,
+    JUMPING_FORWARD,
+    DASH_JUMPING_BACK,
+    DASH_JUMPING_FORWARD,
+    AIRDASHING_BACK,
+    AIRDASHING_FORWARD,
+    ATTACKING // This will probably change to a bunch of different attacks
+}

356b6012f30986d2ece1df5c8fba2282b06150d9
PeachTreeOath <peachtreeoath@gmail.com>
2017-08-14 22:50:13 -0600

    Prefabify

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    private bool isFacingRight;
+    private Rigidbody2D body;
+
 	// Use this for initialization
 	void Start () {
-		
+        body = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+
+        }
+    }
 }
